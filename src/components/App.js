71aad90c2a81610cb8599061e0f8820fcafe5292
Omodiagbe Heart <omodiagbejoshua6@gmail.com>
2024-03-01 16:25:43 -1100

    main quiz

---
+++ src/components/App.js
@@ -0,0 +1,220 @@
+import { useEffect, useReducer } from "react";
+
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+import PreviousButton from "./PreviousButton";
+
+const SECS_PER_QUESTION = 30;
+
+const initialState = {
+  questions: [],
+
+  filterQuestions: [],
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading",
+  index: 0,
+  answer: null,
+  answers: [],
+  points: 0,
+  highscore: 0,
+  secondsRemaining: null,
+  difficulty: "all",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+        filterQuestions: action.payload,
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.filterQuestions.length * SECS_PER_QUESTION,
+      };
+    case "newAnswer":
+      const question = state.filterQuestions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+        answers: [...state.answers, action.payload],
+      };
+
+    case "nextQuestion":
+      const iterator =
+        state.index + 1 < state.filterQuestions.length
+          ? state.index + 1
+          : state.index;
+
+      return {
+        ...state,
+        index: iterator,
+        answer: state.answers.at(iterator) ? state.answers.at(iterator) : null,
+      };
+    case "prevQuestion":
+      const index = state.index - 1 >= 0 ? state.index - 1 : state.index;
+
+      return {
+        ...state,
+        index,
+        answer: state.answers.at(index) ? state.answers.at(index) : null,
+      };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "reset":
+      return {
+        ...initialState,
+        status: "ready",
+        highscore: state.highscore,
+        questions: state.questions,
+        filterQuestions: state.filterQuestions,
+        difficulty: state.difficulty,
+      };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+    case "seeAnswers":
+      return { ...state, answer: state.answers[state.index], status: "verify" };
+    case "setDifficulty":
+      return {
+        ...state,
+        difficulty: action.payload,
+        filterQuestions:
+          action.payload === "all"
+            ? state.questions
+            : state.questions.filter(
+                (question) => question.difficulty === action.payload
+              ),
+      };
+
+    default:
+      throw new Error("Action unkonwn");
+  }
+}
+
+function App() {
+  const [
+    {
+      questions,
+
+      status,
+      index,
+      answer,
+      points,
+      highscore,
+      secondsRemaining,
+      difficulty,
+      filterQuestions,
+    },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  const numQuestions = filterQuestions.length;
+  const maxPossiblePoints = filterQuestions.reduce(
+    (acc, question) => acc + question.points,
+    0
+  );
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen
+            numQuestions={numQuestions}
+            dispatch={dispatch}
+            difficulty={difficulty}
+          />
+        )}
+        {(status === "active" || status === "verify") && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={filterQuestions.length}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={filterQuestions.at(index)}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              {status !== "verify" && (
+                <Timer
+                  dispatch={dispatch}
+                  secondsRemaining={secondsRemaining}
+                />
+              )}
+              {status === "verify" && (
+                <PreviousButton dispatch={dispatch} index={index} />
+              )}
+
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                numQuestions={filterQuestions.length}
+                index={index}
+                status={status}
+              />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
